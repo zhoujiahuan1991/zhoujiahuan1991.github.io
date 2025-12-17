5745a9afe3532b613a13533ee9e505a56594ac4f
azx <zxAi@qq.com>
2025-12-17 14:22:45 +0800

    update

--- common/tools/resize_photo.py
+++ common/tools/resize_photo.py
@@ -45,7 +45,7 @@ def resize_crop_or_pad(image_path,
 
 
 # ========= 使用示例 =========
-image_path  = 'common/img/xyk.jpg'
+image_path  = 'common/img/czy.jpg'
 output_path = image_path.replace('.jpg', '-resized.jpg')
 resize_crop_or_pad(image_path,
                    output_path,
